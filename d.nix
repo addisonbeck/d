a4bf834478850268940626d92afe1f2e744bd305
Addison Beck <github@addisonbeck.com>
2024-09-13 15:54:31 -0400

    Initial commit

---
+++ d.nix
@@ -0,0 +1,22 @@
+{ stdenv, pkgs }:
+
+let version = "0.0.1";
+in stdenv.mkDerivation {
+
+  name = "d-${version}";
+
+  src = ./src;
+
+  nativeBuildInputs = [ ];
+  buildInputs = [ pkgs.makeWrapper ];
+
+  buildPhase = "";
+
+  installPhase = ''
+    cp -r $src $out
+  '';
+
+  # postFixup = ''
+  #   wrapProgram $out/bin/d --prefix PATH : $out/bin
+  # '';
+}
