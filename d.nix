08f82a0a5a8fe1e453864e82dcd74d79d7eec58e
Addison Beck <github@addisonbeck.com>
2024-09-15 13:35:11 -0400

    .

--- d.nix
+++ d.nix
@@ -12,10 +12,9 @@ in stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/d
     cp -r $src/* $out/d
-    install -D $src/bin/d $out/bin/d
   '';
 
   postFixup = ''
-    wrapProgram $out/bin/d --prefix PATH : $out/bin
+    wrapProgram $out/d/bin/d --prefix PATH : $out/bin
   '';
 }
